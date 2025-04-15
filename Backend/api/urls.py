5f835f338c3bd341fce5f52a3170986399258d19
Yordy <iYord@protonmail.com>
2025-04-14 23:47:00 -0400

    Add Tasks AI

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('tasks/', views.TaskListCreateView.as_view(), name='tasks-list'),
     path('task/delete/<int:pk>/', views.TaskDelete.as_view(), name='task-delete'),
     path('chatgpt/', views.ChatGPTView.as_view(), name='chatgpt'),
-    path('genai/', views.GenAIView.as_view(), name='genai'),
 ]
